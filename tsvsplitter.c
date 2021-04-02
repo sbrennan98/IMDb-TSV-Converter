7f23f2cf2338559153f8677cf2fa3dc4ed33d10f
Sean Brennan <brennansean6@gmail.com>
2021-04-02 13:22:44 -0500

    More changes to readme & source file

--- tsvsplitter.c
+++ tsvsplitter.c
@@ -2,14 +2,56 @@
 #include <string.h>
 
 #define ROW_LIMIT 1048576 //Number of rows microsoft excel can comprehend
+#define STRING_LENGTH 20
+#define NUM_TITLES_BUFFER 10
 
-int split(int lines);
+//Get all the different titleTypes of every entry
+char** getTitles(size_t *numTitles, FILE* fp);
 
 int main(void){
     FILE *fp = fopen("data.tsv", "r");
-    printf("Movie Number: %s\n", "WIP");
+    //Number of titles*STRING_LENGTH characters
+    //size_t numTitles = titleTypes();
+    //char **titles = malloc(numTitles*sizeof(char)*STRING_LENGTH);
+    size_t numTitles = 0;
+    char **titles = getTitles(&numTitles, fp);
+    
+
+    printf("There are %zu titles present in this dataset.\n", numTitles);
+    for(size_t i = 0; i < numTitles; i++){
+        printf("Title %zu: %s\n", titles[i]);
+    }
 }
 
-int split(int lines){
+char** getTitles(size_t *numTitles, FILE* fp){
+    //Make sure we're at the beginning
+    rewind(fp);
+    //Go to newline to skip the column titles
+    //Source: https://stackoverflow.com/questions/30065675/what-does-scanf-nc-mean
+    fscanf(fp, "%*[^\n]");
 
+    char titles[NUM_TITLES_BUFFER][STRING_LENGTH];
+    size_t numTitlesCounter = 0;
+
+    while(fp != feof(fp)){
+        //Skip tconst
+        char tconst[STRING_LENGTH];
+        //Get titleType
+        char titleType[STRING_LENGTH];
+        fscanf(fp, "%s  %s", tconst, titleType);
+
+        //Compare titleType to current entries
+        for(int i = 0; i < NUM_TITLES_BUFFER; i++){
+            if(strcmp(titleType, titles[i]) == 0){ //Title is already in list
+                //Go to newline
+                fscanf(fp, "%*[^\n]");
+                continue;
+            }
+        }
+
+        //Go to newline
+        fscanf(fp, "%*[^\n]");
+    }
+
+    return titles;
 }
